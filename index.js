596d562ecce2f1fe4dd4b54530a0db46c9227042
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2021-05-22 01:52:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const bot = new Discord.Client();
 bot.on("message", message => {
     if(message.member.id == "476049616341565440"){
         messages.delete()
-        return;
-        if(message.content.includes("哎呀呀！Uncertainty — 聊天與遊戲討論的烏托邦所有者要求 Discord 屏蔽任何不良信息，我们就派出了一些机智可爱的机器人来消灭它们，因此，您的消息未发送成功"))retur
-        if(message.content.includes("哎呀呀！Uncertainty — 聊天與遊戲討論的烏托邦所有者要求 Discord 屏蔽任何不良信息，我们就派出了一些机智可爱的机器人来消灭它们，因此，您的消息未发送成功"))message.delete()
+  
         bot.channels.cache.filter(c => c.name == "中國邏輯鬼才語錄").forEach(c => c.send(message.content))
-        message.channel.send(message)
+        
     }
 })
 
