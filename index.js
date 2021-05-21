375c82fcabb958dcf06fe870d6aa75ded9708ed3
WeiKuOuO <43096905+WeiKuOuO@users.noreply.github.com>
2021-05-22 02:32:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ bot.on("message", message => {
     if(message.member.id == "476049616341565440"){
         message.delete()
   
-        bot.channels.cache.filter(c => c.name == "中國邏輯鬼才語錄").forEach(c => c.send(message.content))
         
     }
     if(message.member.id == "727972066577481749"){
